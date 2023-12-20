49bcf075bc0e517116a08b3fc7636791279db713
Luís Henrique <henrique2345@ufg.br>
2023-12-20 02:00:18 -0300

    Configurando admin e implementando permissões

--- appcaduser/views/api.py
+++ appcaduser/views/api.py
@@ -1,5 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsOwner
 
 from ..models import Usuario
 from ..serializers import UserSerializer
@@ -12,5 +15,10 @@ class UserApiListViewSet(ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UserSerializer
     pagination_class = Paginacao
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(), ]
+        return super().get_permissions()
 
--- cadastrarusuario/urls.py
+++ cadastrarusuario/urls.py
@@ -1,5 +1,7 @@
 from appcaduser import views
 
+from django.contrib import admin
+
 from django.urls import path, include
 
 from rest_framework.routers import SimpleRouter
@@ -18,6 +20,7 @@ user_api_router.register(
 
 urlpatterns = [
    
+    path('admin/', admin.site.urls),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
